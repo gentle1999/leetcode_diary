ec9f1377f66093686749bb4ff0cba355c9efa7da
gentle1999 <50197885+gentle1999@users.noreply.github.com>
2022-01-19 22:49:25 +0800

    Create 1.two-sum.py

---
+++ id_1_to_10/1.two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        index_map = dict()
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            try:
+                return [index_map[diff], i]
+            except:
+                index_map[nums[i]] = i
